d25358e92b75c49377370ac8429db1a23eedb3e6
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-11-07 14:38:20 +0100

    rewrite class components to functional

--- App/screens/CreateAccount.js
+++ App/screens/CreateAccount.js
@@ -1,117 +1,95 @@
-import React from "react";
-import {
-  ScrollView,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  View
-} from "react-native";
+import React, { useState } from 'react'
+import { ScrollView, Text, TouchableOpacity, StyleSheet, View } from 'react-native'
 
-import { TextField, ErrorText } from "../components/Form";
-import { Button } from "../components/Button";
+import { TextField, ErrorText } from '../components/Form'
+import { Button } from '../components/Button'
 
 const styles = StyleSheet.create({
   textBlock: {
-    marginTop: 20
+    marginTop: 20,
   },
   text: {
     fontSize: 18,
-    color: "#969696",
-    textAlign: "center",
-    marginBottom: 2
+    color: '#969696',
+    textAlign: 'center',
+    marginBottom: 2,
   },
   link: {
-    textDecorationLine: "underline"
-  }
-});
+    textDecorationLine: 'underline',
+  },
+})
 
 const isValidInputs = state => {
-  const fields = ["email", "fName", "lName", "password", "cPassword"];
+  const fields = ['email', 'fName', 'lName', 'password', 'cPassword']
   const validArray = fields.map(field => {
     if (!state[field] || state[field].length === 0) {
-      return false;
+      return false
     }
-    return true;
-  });
+    return true
+  })
 
-  const validFields = validArray.filter(valid => valid);
-  return validFields.length === fields.length;
-};
+  const validFields = validArray.filter(valid => valid)
+  return validFields.length === fields.length
+}
 
-export default class CreateAccount extends React.Component {
-  state = { errorMessage: null };
+const CreateAccount = ({ navigation }) => {
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [email, setEmail] = useState('')
+  const [fName, setFName] = useState('')
+  const [lName, setLName] = useState('')
+  const [password, setPassword] = useState('')
+  const [cPassword, setCPassword] = useState('')
 
-  onSubmit = () => {
-    if (!isValidInputs(this.state)) {
-      this.setState({ errorMessage: "An error occured." });
+  const onSubmit = () => {
+    if (!isValidInputs(email && fName && lName && password && cPassword)) {
+      setErrorMessage('An error occured.')
     } else {
-      this.setState({ errorMessage: null });
-      const { email, fName, lName, password } = this.state;
+      setErrorMessage(null)
 
-      fetch("https://postman-echo.com/post", {
-        method: "POST",
+      fetch('https://postman-echo.com/post', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json"
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           email,
           fName,
           lName,
-          password
-        })
+          password,
+        }),
       })
         .then(res => res.json())
         .then(res => {
-          console.log("res", res);
+          console.log('res', res)
         })
         .catch(err => {
-          console.log("err", err);
-        });
+          console.log('err', err)
+        })
     }
-  };
-
-  render() {
-    const { navigation } = this.props;
-
-    return (
-      <ScrollView
-        contentContainerStyle={{ paddingVertical: 20 }}
-        style={{ backgroundColor: "#fff" }}
-      >
-        <TextField
-          label="Email"
-          placeholder="john.doe@example.com"
-          onChangeText={email => this.setState({ email })}
-        />
-        <TextField
-          label="First Name"
-          placeholder="John"
-          onChangeText={fName => this.setState({ fName })}
-        />
-        <TextField
-          label="Last Name"
-          placeholder="Doe"
-          onChangeText={lName => this.setState({ lName })}
-        />
-        <TextField
-          label="Password"
-          secureTextEntry
-          onChangeText={password => this.setState({ password })}
-        />
-        <TextField
-          label="Confirm Password"
-          secureTextEntry
-          onChangeText={cPassword => this.setState({ cPassword })}
-        />
-        <ErrorText text={this.state.errorMessage} />
-        <Button text="Submit" onPress={this.onSubmit} />
-        <View style={styles.textBlock}>
-          <Text style={styles.text}>Already have an account?</Text>
-          <TouchableOpacity onPress={() => navigation.navigate("SignIn")}>
-            <Text style={[styles.text, styles.link]}>Sign in.</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-    );
   }
+
+  return (
+    <ScrollView contentContainerStyle={{ paddingVertical: 20 }} style={{ backgroundColor: '#fff' }}>
+      <TextField
+        label="Email"
+        placeholder="john.doe@example.com"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextField label="First Name" placeholder="John" value={fName} onChangeText={setFName} />
+      <TextField label="Last Name" placeholder="Doe" value={lName} onChangeText={setLName} />
+      <TextField label="Password" value={password} onChangeText={setPassword} />
+      <TextField label="Confirm Password" value={cPassword} onChangeText={setCPassword} />
+      <ErrorText text={errorMessage} />
+      <Button text="Submit" onPress={onSubmit} />
+      <View style={styles.textBlock}>
+        <Text style={styles.text}>Already have an account?</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>
+          <Text style={[styles.text, styles.link]}>Sign in.</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  )
 }
+
+export default CreateAccount
--- App/screens/SignIn.js
+++ App/screens/SignIn.js
@@ -1,44 +1,39 @@
-import React from "react";
-import { ScrollView } from "react-native";
+import React, { useState } from 'react'
+import { ScrollView } from 'react-native'
 
-import { TextField, ErrorText } from "../components/Form";
-import { Button } from "../components/Button";
+import { TextField, ErrorText } from '../components/Form'
+import { Button } from '../components/Button'
 
-export default class SignIn extends React.Component {
-  state = {
-    email: "",
-    password: "",
-    error: ""
-  };
+const SignIn = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
 
-  handleSubmit = () => {
-    this.setState({ error: "" });
-    alert("todo!");
-  };
-
-  render() {
-    return (
-      <ScrollView
-        contentContainerStyle={{ paddingVertical: 20 }}
-        style={{ backgroundColor: "#fff" }}
-      >
-        <TextField
-          label="Email"
-          placeholder="john.doe@example.com"
-          onChangeText={email => this.setState({ email })}
-          value={this.state.email}
-          autoCapitalize="none"
-        />
-        <TextField
-          label="Password"
-          secureTextEntry
-          onChangeText={password => this.setState({ password })}
-          value={this.state.password}
-          autoCapitalize="none"
-        />
-        <ErrorText text={this.state.error} />
-        <Button text="Submit" onPress={this.handleSubmit} />
-      </ScrollView>
-    );
+  const handleSubmit = () => {
+    setError('')
+    alert('todo!')
   }
+
+  return (
+    <ScrollView contentContainerStyle={{ paddingVertical: 20 }} style={{ backgroundColor: '#fff' }}>
+      <TextField
+        label="Email"
+        placeholder="john.doe@example.com"
+        onChangeText={setEmail}
+        value={email}
+        autoCapitalize="none"
+      />
+      <TextField
+        label="Password"
+        secureTextEntry
+        onChangeText={setPassword}
+        value={password}
+        autoCapitalize="none"
+      />
+      <ErrorText text={error} />
+      <Button text="Submit" onPress={handleSubmit} />
+    </ScrollView>
+  )
 }
+
+export default SignIn
