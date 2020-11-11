d56dbfd668a1327cb0ebdaa6370032f0d81fb126
MateuszRostkowski <mateusz.rostkowsky995@gmail.com>
2020-11-11 22:48:35 +0100

    add test cases for Counter component

--- App/index.test.js
+++ App/index.test.js
@@ -1,3 +1,19 @@
-test('it works', () => {
-  expect(true).toBeTruthy()
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react-native'
+import { Counter } from './components/Counter'
+
+test('Render corectly', () => {
+  const { toJSON } = render(<Counter />)
+
+  expect(toJSON()).toMatchSnapshot()
+})
+
+test('Test counter', async () => {
+  const { getByTestId } = render(<Counter />)
+  const buttonText = getByTestId('buttonText')
+
+  fireEvent.press(buttonText)
+  expect(buttonText.props.children).toBe(1)
+  fireEvent.press(buttonText)
+  expect(buttonText.props.children).toBe(2)
 })
